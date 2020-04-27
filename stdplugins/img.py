700e657ec576d97e2753a8b9df8f2b92bc550b65
mrnobody69 <59581839+mrnobody69@users.noreply.github.com>
2020-04-28 02:47:02 +0530

    Update img.py

--- stdplugins/img.py
+++ stdplugins/img.py
@@ -14,14 +14,14 @@ from uniborg.util import admin_cmd
 
 @borg.on(admin_cmd(pattern="img ?(.*)"))
 async def img_sampler(event):
-    await event.edit("`Processing Bsdk..`")
+    await event.edit("`Processing...`")
     reply = await event.get_reply_message()
     if event.pattern_match.group(1):
         query = event.pattern_match.group(1)
     elif reply:
         query = reply.message
     else:
-    	await event.edit("`What I am Supposed to Search u Dumb Ass(Donkey)`")
+    	await event.edit("`What I am Supposed to Search`")
     	return
         
     lim = findall(r"lim=\d+", query)
