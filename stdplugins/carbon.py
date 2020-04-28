cf21808ce41e95b8cac9329d822e6b217945b8a7
mrnobody69 <59581839+mrnobody69@users.noreply.github.com>
2020-04-28 21:05:53 +0530

    changed

--- stdplugins/carbon.py
+++ stdplugins/carbon.py
@@ -1,9 +1,8 @@
-"""Carbon Scraper Plugin for Userbot. //text in creative way.
+"""
+Carbon Scraper Plugin for Userbot. //text in creative way.
 usage: .carbon //as a reply to any text message
-
-Thanks to @r4v4n4 for vars"""
-
-from selenium.webdriver.common.keys import Keys
+Thanks to @r4v4n4 for vars
+"""
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
@@ -11,15 +10,14 @@ from telethon import events
 from urllib.parse import quote_plus
 from urllib.error import HTTPError
 from time import sleep
-from uniborg.util import admin_cmd
 import asyncio
 import os
 
-@borg.on(admin_cmd(pattern="carbon ?(.*)", allow_sudo=True))
+@borg.on(events.NewMessage(pattern=r"\.carbon", outgoing=True))
 async def carbon_api(e):
  if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
    """ A Wrapper for carbon.now.sh """
-   hmm = await e.reply("⚔️⚔️⚔️⚔️⚔️")
+   await e.edit("⬜⬜⬜⬜⬜")
    CARBON = 'https://carbon.now.sh/?l={lang}&code={code}'
    CARBONLANG = "en"
    textx = await e.get_reply_message()
@@ -39,7 +37,7 @@ async def carbon_api(e):
    chrome_options.add_argument('--disable-gpu')
    prefs = {'download.default_directory' : './'}
    chrome_options.add_experimental_option('prefs', prefs)
-   await hmm.edit("🛡🛡⚔️⚔️⚔️")
+   await e.edit("⬛⬛⬜⬜⬜")
 
    driver = webdriver.Chrome(executable_path=Config.CHROME_DRIVER, options=chrome_options)
    driver.get(url)
@@ -48,27 +46,25 @@ async def carbon_api(e):
    params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_path}}
    command_result = driver.execute("send_command", params)
 
-   driver.find_element_by_id("export-menu").click()
-
-   #driver.find_element_by_xpath("//button[contains(text(),'Export')]").click()
+   driver.find_element_by_xpath("//button[contains(text(),'Export')]").click()
    sleep(5) # this might take a bit.
    driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
    sleep(5)
-   await hmm.edit("🛡🛡🛡⚔️⚔️")
+   await e.edit("⬛⬛⬛⬜⬜")
    driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
    sleep(5) #Waiting for downloading
 
-   await e.edit("🛡🛡🛡🛡🛡")
+   await e.edit("⬛⬛⬛⬛⬛")
    file = './carbon.png'
-   await hmm.edit("📛Carbon Completed,☣️Uploading Carbon⬆️")
+   await e.edit("✅Carbon Completed, Uploading Carbon✅")
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="Carbon by [@PhycoNinja13b](https://github.com/Phyco-Ninja/UniNinja)",
-         force_document=True,
+         caption="Carbon by [@r4v4n4](https://www.github.com/ravana69/pornhub)",
+         force_document=False,
          reply_to=e.message.reply_to_msg_id,
          )
 
    os.remove('./carbon.png')
    # Removing carbon.png after uploading
-   await hmm.delete() # Deleting msg
+   await e.delete() # Deleting msg
