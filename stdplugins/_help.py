2d766a47ef2c0db2896d907b00e8aaf2f717c999
mrnobody69 <59581839+mrnobody69@users.noreply.github.com>
2020-05-08 20:15:38 +0530

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -1,9 +1,21 @@
+"""**Know Your UniBorg**
+◇ list of all loaded plugins
+◆ `.helpme`\n
+◇ to know Data Center
+◆ `.dc`\n
+◇ powered by
+◆ `.config`\n
+◇ to know syntax
+◆ `.syntax` <plugin name>
+"""
+
+
 import sys
 from telethon import events, functions, __version__
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="helpme ?(.*)", allow_sudo=True))  # pylint:disable=E0602
+@borg.on(admin_cmd(pattern="help ?(.*)", allow_sudo=True))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
@@ -12,10 +24,10 @@ async def _(event):
         s_help_string = borg._plugins[splugin_name].__doc__
     else:
         s_help_string = ""
-    help_string = """=Hi, I am a Bot in service of Master Alex. I am Running.. 
-Python {}
-Telethon {}`
-""".format(
+    help_string = """UserBot is alive!
+**Python:** `{}`
+**Telethon:** `{}`
+**Owner: Alex**""".format(
         sys.version,
         __version__
     )
@@ -51,4 +63,21 @@ async def _(event):
     result = await borg(functions.help.GetConfigRequest())  # pylint:disable=E0602
     result = result.stringify()
     logger.info(result)  # pylint:disable=E0602
-    await event.edit("""Telethon UserBot powered by @UniBorg""")
+    await event.edit("""Telethon UserBot powered by @ViperAdnan""")
+
+
+@borg.on(admin_cmd(pattern="syntax (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    plugin_name = event.pattern_match.group(1)
+    if plugin_name in borg._plugins:
+        help_string = borg._plugins[plugin_name].__doc__
+        unload_string = f"Use `.unload {plugin_name}` to remove this plugin.\n           © @UniBorg"
+        if help_string:
+            plugin_syntax = f"**Syntax for plugin `{plugin_name}`**:\n\n{help_string}\n{unload_string}"
+        else:
+            plugin_syntax = f"**No DOCSTRING has been setup for {plugin_name} plugin.**"
+    else:
+        plugin_syntax = "**Enter valid Plugin name.\nDo `.exec ls stdplugins` or `.help` to get list of valid plugin names.**"
+    await event.edit(plugin_syntax)
